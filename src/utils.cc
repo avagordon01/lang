52c4c41be04d75e3b667880d55518d826b086f00
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-02-02 01:28:45 +0000

    better error location printing

--- src/utils.cc
+++ src/utils.cc
@@ -8,11 +8,12 @@
 #include <memory>
 
 void error(std::string s) {
-    std::cout << s << std::endl;
+    std::cerr << s << std::endl;
     exit(1);
 }
 void yy::parser::error(const location_type& l, const std::string& m) {
-    std::cerr << l << ": " << m << std::endl;
+    std::cerr << "line " << l.begin.line << " column " << l.begin.column << ": " << m << std::endl;
+    exit(1);
 }
 
 static std::unordered_map<std::string, ast::identifier> symbols;
