3308b29bac1c7592662428157957b2f0e1ec420e
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-04-19 16:52:34 +0100

    neater pair access

--- src/scopes.hh
+++ src/scopes.hh
@@ -22,11 +22,11 @@ public:
     std::optional<T> find_item_current_scope(ID id) {
         auto l = std::find_if(data.rbegin(), data.rbegin() + sizes.back(),
             [id](std::pair<ID, T> x) -> bool {
-                return std::get<0>(x) == id;
+                return x.first == id;
             }
         );
         if (l != data.rbegin() + sizes.back()) {
-            return {std::get<1>(*l)};
+            return l->second;
         } else {
             return std::nullopt;
         }
@@ -34,11 +34,11 @@ public:
     std::optional<T> find_item(ID id) {
         auto l = std::find_if(data.rbegin(), data.rend(),
             [id](std::pair<ID, T> x) -> bool {
-                return std::get<0>(x) == id;
+                return x.first == id;
             }
         );
         if (l != data.rend()) {
-            return {std::get<1>(*l)};
+            return l->second;
         } else {
             return std::nullopt;
         }
