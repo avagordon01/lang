c803e6d68cfc05772acc314d029a668e2c461374
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-06-09 21:12:16 +0100

    add initial context

--- src/codegen_spirv.cc
+++ src/codegen_spirv.cc
@@ -2,3 +2,10 @@
 #include "ast.hh"
 #include "codegen_spirv.hh"
 #include "error.hh"
+
+//TODO
+//example GlslangToSpv.cpp
+//documentation SpvBuilder.h
+
+void codegen_spirv(codegen_context_spirv &context, ast::program &program, const std::string& src_filename, const std::string& ir_filename) {
+}
--- src/codegen_spirv.hh
+++ src/codegen_spirv.hh
@@ -1,5 +1,19 @@
+#include <memory>
+
 #include <SPIRV/GlslangToSpv.h>
 #include <SPIRV/SpvBuilder.h>
-//TODO
-//example GlslangToSpv.cpp
-//documentation SpvBuilder.h
+
+#include "scopes.hh"
+#include "ast.hh"
+
+struct codegen_context_spirv {
+    spv::Builder builder;
+    spv::Module module;
+    ::scopes<ast::identifier, size_t> variable_scopes;
+    std::vector<std::string> symbols_list;
+    spv::Block* current_function_entry = NULL;
+    spv::Block* current_loop_exit = NULL;
+    spv::Block* current_loop_entry = NULL;
+};
+
+void codegen_spirv(codegen_context_spirv &context, ast::program &program, const std::string& src_filename, const std::string& ir_filename);
