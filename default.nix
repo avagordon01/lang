0569e68dc8b858f000eb5db37e71a793b3a0c0bf
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-01-11 13:26:46 +0000

    switch to meson

--- default.nix
+++ default.nix
@@ -4,10 +4,5 @@ stdenv.mkDerivation {
   name = "lang";
   src = ./.;
 
-  buildInputs = [ flex bison llvm_8 glslang ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp out/compiler $out/bin
-  '';
+  buildInputs = [ meson ninja flex bison llvm_8 glslang ];
 }
