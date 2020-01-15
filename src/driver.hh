fcb914aa6121d61d7f8eaad33c5b6b66eed61a83
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-01-15 22:33:10 +0000

    split driver

--- src/driver.hh
+++ src/driver.hh
@@ -1,10 +1,7 @@
-#include <cstdio>
-#include <cstring>
 #include <string>
 
 #include "ast.hh"
 #include "alt-parser.hh"
-#include "error.hh"
 #define YY_DECL token_type yylex(driver& drv)
 YY_DECL;
 extern FILE *yyin;
@@ -16,22 +13,8 @@ struct driver {
     std::unordered_map<std::string, ast::identifier> symbols_map;
     std::vector<std::string> symbols_list;
     std::string filename;
-    void parse() {
-        location.initialize(&filename);
-        scan_begin();
-        parser_context parser(*this);
-        parser.parse_program();
-        scan_end();
-    }
 
-    void scan_begin() {
-        yyin = fopen(filename.c_str(), "r");
-        if (!yyin) {
-            error("cannot open", filename, ":", std::strerror(errno));
-        }
-    }
-
-    void scan_end() {
-        fclose(yyin);
-    }
+    void parse();
+    void scan_begin();
+    void scan_end();
 };
