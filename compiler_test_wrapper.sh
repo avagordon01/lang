2e6021b225773a5a76174a39b4bab3c674a1b11b
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-02-09 20:47:01 +0000

    add script to print ast and ir

--- compiler_test_wrapper.sh
+++ compiler_test_wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
 
 type="$1"
 
@@ -8,8 +8,25 @@ input_raw="$2"
 input="${input_raw%.*}"
 output_raw="$(basename ${input}).ir"
 output="${output_raw%.*}"
+print_ast=true
+print_ir=true
 
-./compiler ${input_raw} ${output_raw}
+if [ "$print_ast" = true ]; then
+    gdb -ex "break main.cc:15" \
+        -ex run \
+        -ex "print driver.program_ast" \
+        -ex continue \
+        -ex quit \
+        --args \
+        ./compiler ${input_raw} ${output_raw}
+else
+    ./compiler ${input_raw} ${output_raw}
+fi
+
+if [ "$print_ir" = true ]; then
+    cat ${output_raw}
+fi
+
 if [ $type != "parse" ]; then
     llc -filetype=obj ${output_raw} -o ${output}.o
     if [ $type == "exe" ]; then
