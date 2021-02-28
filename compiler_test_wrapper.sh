161b133b7b217d8ba3cf2de2cf63d2d2e1c52f77
Patrick Gordon <pgpatrickgordon@gmail.com>
2021-02-28 20:16:49 +0000

    remove old compiler_test_wrapper.sh debug prints

--- compiler_test_wrapper.sh
+++ compiler_test_wrapper.sh
@@ -8,24 +8,8 @@ input_raw="$2"
 input="${input_raw%.*}"
 output_raw="$(basename ${input}).ir"
 output="${output_raw%.*}"
-print_ast=false
-print_ir=false
 
-if [ "$print_ast" = true ]; then
-    gdb -ex "break main.cc:15" \
-        -ex run \
-        -ex "print driver.program_ast" \
-        -ex continue \
-        -ex quit \
-        --args \
-        ./compiler ${input_raw} ${output_raw}
-else
-    ./compiler ${input_raw} ${output_raw}
-fi
-
-if [ "$print_ir" = true ]; then
-    cat ${output_raw}
-fi
+./compiler ${input_raw} ${output_raw}
 
 if [ $type != "parse" ]; then
     llc -filetype=obj ${output_raw} -o ${output}.o
